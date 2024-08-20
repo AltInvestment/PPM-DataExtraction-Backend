0c57dd2e8a84fab064691e0c164b00f219c88229
Vagelie <altinvestppmreviewer@gmail.com>
2024-08-20 19:54:57 +0100

    Add a colorized logging utility for better readability

---
+++ utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+import colorlog
+
+def setup_logger():
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(
+        '%(log_color)s%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        }
+    ))
+    logger = colorlog.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
+
+logger = setup_logger()
