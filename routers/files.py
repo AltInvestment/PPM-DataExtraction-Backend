4b5a2d5c8d42539ca1bd2e9e2f8247a44800dabe
Vagelie <altinvestppmreviewer@gmail.com>
2024-10-22 13:43:12 +0100

    Enhance error handling and documentation for file routes
    Improved the `/files` endpoint by adding detailed error handling to gracefully manage exceptions when retrieving processed file IDs and when triggering file processing. This will prevent crashes and provide clearer feedback in case of internal server errors. Updated the API documentation to better describe the functionality of the endpoints, making it easier for users to understand their purpose and expected behavior.

--- routers/files.py
+++ routers/files.py
@@ -1,22 +1,49 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from services.file_processor import process_new_files
 from utils.common import load_processed_file_ids
 from services.google_services import get_services
 from models.schemas import ProcessResponse, FilesResponse
+from utils.logger import logger
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/files", tags=["Files"], responses={404: {"description": "Not found"}}
+)
 
 
-@router.get("/files", response_model=FilesResponse)
+@router.get(
+    "/",
+    response_model=FilesResponse,
+    summary="Get Processed Files",
+    description="Retrieve a list of all processed file IDs.",
+)
 async def get_processed_files():
-    processed_file_ids = load_processed_file_ids("processed_files.txt")
-    return {"processed_file_ids": list(processed_file_ids)}
+    """
+    Retrieve a list of all processed file IDs.
+    """
+    try:
+        processed_file_ids = load_processed_file_ids("processed_files.txt")
+        return {"processed_file_ids": list(processed_file_ids)}
+    except Exception as e:
+        logger.error(f"Error getting Processed files: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error.")
 
 
-@router.post("/process", response_model=ProcessResponse)
+@router.post(
+    "/process",
+    response_model=ProcessResponse,
+    summary="Trigger File Processing",
+    description="Manually trigger the file processing job to check for new files and process them.",
+)
 async def trigger_processing(
     drive_service=Depends(get_services("drive")),
     sheets_service=Depends(get_services("sheets")),
 ):
-    process_new_files(drive_service, sheets_service)
-    return {"message": "File processing triggered."}
+    """
+    Manually trigger the file processing job to check for new files and process them.
+    """
+    try:
+        process_new_files(drive_service, sheets_service)
+        return {"message": "File processing triggered."}
+    except Exception as e:
+        logger.error(f"Error Processing Files: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error.")
